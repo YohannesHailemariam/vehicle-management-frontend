2072f411d793be3670129b3a0f0f665b2f2d3465
iamjohnhm <jhm90822@gmail.com>
2024-12-14 02:13:54 +0300

    Create main App component to render AddVehicleForm and VehicleList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import AddVehicleForm from './components/AddVehicleForm';
+import VehicleList from './components/VehicleList';
 
 function App() {
-      return (
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-  )
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Vehicle Management Dashboard</h1>
+      <AddVehicleForm />
+      <VehicleList />
+    </div>
+  );
 }
 
-export default App
+export default App;
