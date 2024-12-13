4be4021babd8887636e725e2f1822a48878ebe40
iamjohnhm <jhm90822@gmail.com>
2024-12-14 02:06:33 +0300

    Integrate Redux store into React app by wrapping with Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import Provider
+import store from './app/store'; // Import the Redux store
+import './index.css'; // Import global styles (Tailwind CSS)
+import App from './App.jsx'; // Import the App component
 
+// Create the root element and render the App with the Redux Provider
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}> {/* Wrap your app with the Redux Provider */}
+      <App />
+    </Provider>
+  </StrictMode>
+);
